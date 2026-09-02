6395c46fe6e8c32f946e8fd55297193cc41f47fe
dacarc <diegocarcamo92@gmail.com>
2026-09-02 17:08:01 -0400

    Refactor SQL queries for clarity and organization

--- queries.sql
+++ queries.sql
@@ -1,24 +1,56 @@
-#Average Excess Readmission Ratio by State
+-- NATIONAL QUERIES
+
+-- Average Excess Readmission Ratio by State
 SELECT State, AVG("Excess Readmission Ratio") AS avg_excess_readmission_ratio
 FROM hospitals
 GROUP BY State
 ORDER BY avg_excess_readmission_ratio DESC;
 
-#Average Patient Satisfaction by State
+-- Average Patient Satisfaction by State
 SELECT State, AVG("Patient Survey Star Rating") AS avg_patient_survey_rating
 FROM hospitals
 GROUP BY State
 ORDER BY avg_patient_survey_rating DESC;
 
-#Average Excess Readmission Ratio by Condition
+-- Average Excess Readmission Ratio by Condition
 SELECT "Measure Name", AVG("Excess Readmission Ratio") AS avg_excess_readmission_ratio
 FROM hospitals
 GROUP BY "Measure Name"
 ORDER BY avg_excess_readmission_ratio DESC;
 
-#Top 10 Worst Performing Hospitals
+-- Top 10 Worst Performing Hospitals
 SELECT "Facility Name", State, AVG("Excess Readmission Ratio") AS avg_excess_readmission_ratio
 FROM hospitals
 GROUP BY "Facility Name"
 ORDER BY avg_excess_readmission_ratio DESC
 LIMIT 10;
+
+
+-- VIRGINIA QUERIES
+
+-- Top 10 Worst Performing Virginia Hospitals
+SELECT "Facility Name", AVG("Excess Readmission Ratio") AS avg_excess_readmission_ratio
+FROM virginia
+GROUP BY "Facility Name"
+ORDER BY avg_excess_readmission_ratio DESC
+LIMIT 10;
+
+-- Top 10 Best Performing Virginia Hospitals
+SELECT "Facility Name", AVG("Excess Readmission Ratio") AS avg_excess_readmission_ratio
+FROM virginia
+GROUP BY "Facility Name"
+ORDER BY avg_excess_readmission_ratio ASC
+LIMIT 10;
+
+-- Patient Satisfaction by Hospital and Category
+SELECT "Facility Name", "HCAHPS Question", AVG("Patient Survey Star Rating") AS avg_rating
+FROM virginia
+GROUP BY "Facility Name", "HCAHPS Question"
+ORDER BY avg_rating ASC
+LIMIT 10;
+
+-- Readmission Ratio by Condition in Virginia
+SELECT "Measure Name", AVG("Excess Readmission Ratio") AS avg_excess_readmission_ratio
+FROM virginia
+GROUP BY "Measure Name"
+ORDER BY avg_excess_readmission_ratio DESC;
